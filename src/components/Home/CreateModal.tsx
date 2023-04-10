14e3e5929a608fcf10abd9cb00b8374f3e832b26
jlchuun <jlchuun@gmail.com>
2023-04-09 17:22:32 -0700

    :feat: initial modal styling

---
+++ src/components/Home/CreateModal.tsx
@@ -0,0 +1,54 @@
+import styles from "./CreateModal.module.scss";
+import { useState } from "react";
+
+const CreateModal = () => {
+    const [open, setOpen] = useState(false);
+
+    const toggleModal = () => {
+        setOpen(!open);
+    };
+
+    return (
+        <div>
+            <button type="button" onClick={toggleModal}>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                    <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+                </svg>
+                Create New Project
+            </button>
+            {open && (
+                <div className={styles.modalOverlay}>
+                    <div className={styles.modalContainer}>
+                        <h2>Create a New Project</h2>
+                        <p>
+                            Dolore amet fugiat id ad et minim eu duis elit ad.
+                            Enim incididunt consequat nisi aute in in veniam.
+                            Occaecat adipisicing quis laborum ea sunt officia
+                            mollit elit et reprehenderit quis anim deserunt ad.
+                            Et tempor aliquip cillum tempor qui ipsum mollit ut
+                            nisi Lorem nostrud pariatur veniam. Ea minim magna
+                            aute ad excepteur dolor incididunt laborum.
+                            Excepteur amet aliqua quis ex voluptate est ipsum
+                            enim ullamco tempor officia consequat ullamco culpa.
+                            Est laborum cillum ad et quis sunt pariatur do.
+                        </p>
+                        <button
+                            type="button"
+                            onClick={toggleModal}
+                            className={styles.closeBtn}
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 512 512"
+                            >
+                                <path d="M464 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h416c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-83.6 290.5c4.8 4.8 4.8 12.6 0 17.4l-40.5 40.5c-4.8 4.8-12.6 4.8-17.4 0L256 313.3l-66.5 67.1c-4.8 4.8-12.6 4.8-17.4 0l-40.5-40.5c-4.8-4.8-4.8-12.6 0-17.4l67.1-66.5-67.1-66.5c-4.8-4.8-4.8-12.6 0-17.4l40.5-40.5c4.8-4.8 12.6-4.8 17.4 0l66.5 67.1 66.5-67.1c4.8-4.8 12.6-4.8 17.4 0l40.5 40.5c4.8 4.8 4.8 12.6 0 17.4L313.3 256l67.1 66.5z" />
+                            </svg>
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CreateModal;
--- src/components/Home/Sidebar.tsx
+++ src/components/Home/Sidebar.tsx
@@ -1,12 +1,12 @@
 import styles from "./Sidebar.module.scss";
 import { Project } from "./Home";
-
+import CreateModal from "./CreateModal";
 type SidebarProps = {
     projects: Array<Project>;
     setTabIndex: (index: number) => void;
 };
 
-const Sidebar = ({ projects, setTabIndex }: SidebarProps) => {
+const Sidebar: React.FC<SidebarProps> = ({ projects, setTabIndex }) => {
     return (
         <aside className={styles.sidebar}>
             <ul>
@@ -29,15 +29,7 @@ const Sidebar = ({ projects, setTabIndex }: SidebarProps) => {
                     </li>
                 ))}
                 <li className={styles.addProject}>
-                    <button type="button">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 448 512"
-                        >
-                            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
-                        </svg>
-                        Create New Project
-                    </button>
+                    <CreateModal />
                 </li>
                 <li className={styles.logout}>
                     <button>
